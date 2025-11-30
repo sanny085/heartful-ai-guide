434a97a61646d69aba4108a9f40cb8efed7bd818
sanny085 <sannykumar085@gmail.com>
2025-11-30 21:22:44 +0530

    Tailwindcss issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 export default {
   darkMode: ["class"],
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}", "./app/**/*.{js,jsx}", "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
